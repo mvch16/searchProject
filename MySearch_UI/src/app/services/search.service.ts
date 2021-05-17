5c07018fe57d09d25b7030f6e5b1582ffddd7fbe
Michael <maicol_v16@hotmail.com>
2021-05-16 18:43:54 -0600

    Refactoring API URL

--- MySearch_UI/src/app/services/search.service.ts
+++ MySearch_UI/src/app/services/search.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
 
 export class SearchService {
   constructor(private http: HttpClient) {}
 
   Search(searchQuery: string, pageNumber: number, pageSize: number) {
     return this.http.get(
-      `https://localhost:44306/Search/${searchQuery}/${pageNumber}/${pageSize}`
+      environment.baseURL + `${searchQuery}/${pageNumber}/${pageSize}`
     );
   }
 }
